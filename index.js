4859740de1b37a638be544284a5a302c6e9eb450
Mohammad Sumon <94286474+Mohammad-Sumon@users.noreply.github.com>
2021-11-28 12:43:40 +0600

    added

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var marks = parseFloat(prompt("Enter your marks : "));
+
+if (marks >= 80 && marks <= 100){
+    console.log("A+");
+}
+else if (marks >= 70 && marks <= 79){
+    console.log("A");
+}
+else if (marks >= 60 && marks <= 69){
+    console.log("A-");
+}
+else if (marks >= 50 && marks <= 59){
+    console.log("B");
+}
+else if (marks >= 40 && marks <= 49){
+    console.log("C");
+}
+else if (marks >= 33 && marks <= 39){
+    console.log("D");
+}
+else if (marks >= 101 || marks <= 0){
+    console.log("Not a number");
+}
+else{
+    console.log("F");
+};
